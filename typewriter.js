b498445d7ad83485d0f9ce49a087fe8148281672
Sid Natarajan <sid.nat@gmail.com>
2022-10-25 20:35:23 +0000

    used another setTimeout to create a new line

--- typewriter.js
+++ typewriter.js
@@ -3,9 +3,12 @@ let counter = 0;
 
 for (const char of sentence) {
   setTimeout(() => {
-    // process.stdout.write(char + '\n');
-    console.log(char)
+    process.stdout.write(char);
   }, counter)
 
   counter += 50;
 }
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, counter)
