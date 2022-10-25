c1d2d99d810b670f8dd76c15e737db7cc9205783
Sid Natarajan <sid.nat@gmail.com>
2022-10-25 20:41:43 +0000

    eslint

--- typewriter.js
+++ typewriter.js
@@ -4,11 +4,11 @@ let counter = 0;
 for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
-  }, counter)
+  }, counter);
 
   counter += 50;
 }
 
 setTimeout(() => {
   process.stdout.write('\n');
-}, counter)
+}, counter);
